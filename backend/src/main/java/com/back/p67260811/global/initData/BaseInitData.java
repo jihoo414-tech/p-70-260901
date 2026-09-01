232397392e53f9a676ce0e6a0ee09beb891bda16
jihoo414-tech <jihoo414@gmail.com>
2026-09-01 09:47:37 +0900

    member domain 추가

--- backend/src/main/java/com/back/p67260811/global/initData/BaseInitData.java
+++ backend/src/main/java/com/back/p67260811/global/initData/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.p67260811.global.initData;
 
+import com.back.p67260811.domain.member.service.MemberService;
 import com.back.p67260811.domain.post.post.entity.Post;
 import com.back.p67260811.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,9 @@ public class BaseInitData {
     @Lazy
     private BaseInitData self;
     private final PostService postService;
+    private final MemberService memberService;
+
+
     @Bean
     ApplicationRunner initDataRunner() {
         return args -> {
@@ -27,19 +31,23 @@ public class BaseInitData {
 
     @Transactional
     public void work1() {
-        if (postService.count() > 0) {
+        if (memberService.count() > 0) {
             return;
         }
 
-        Post post1 = postService.write("제목1", "내용1");
-        Post post2 = postService.write("제목2", "내용2");
-        Post post3 = postService.write("제목3", "내용3");
-
-        post1.addComment("댓글 1-1");
-        post1.addComment("댓글 1-2");
-        post1.addComment("댓글 1-3");
-        post2.addComment("댓글 2-1");
-        post2.addComment("댓글 2-2");
+        memberService.join("system", "system", "시스템");
+        memberService.join("admin", "admin", "운영자");
+        memberService.join("user1", "1234", "유저1");
+        memberService.join("user2", "1234", "유저2");
+        memberService.join("user3", "1234", "유저3");
     }
 
+
+    @Transactional
+    public void work2() {
+        if(postService.count() > 0) {
+            return;
+        }
+
+    }
 }
